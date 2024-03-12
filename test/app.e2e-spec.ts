ab2ef91dfe5aa9f1afd037db83d1b9f4f0f0b0aa
Brian <liar88828@gmail.com>
2024-03-12 18:58:53 +0700

    save Integration Test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,35 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "./../src/app.module";
 
-describe('AppController (e2e)', () => {
+describe("AppController (e2e)", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule]
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it("/ (GET)", () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get("/")
       .expect(200)
-      .expect('Hello World!');
+      .expect("Hello World!");
+  });
+
+  it("should can sat hello", async () => {
+    const result = await request(app.getHttpServer())
+      .get("/api/users/hello")
+      .query({
+        first_name:'Eko',
+        last_name:'Kurniawan'
+      });
+    expect(result.status).toBe(200)
+    expect(result.text).toBe('Hello Eko and Kurniawan')
   });
 });
