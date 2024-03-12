b83794f48a8a0e0e50db0eb66144209029bf0874
Brian <liar88828@gmail.com>
2024-03-12 18:50:53 +0700

    save unit test add mock http

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserController } from "./user.controller";
+import * as httpmock from "node-mocks-http";
 
 describe("UserController", () => {
   let controller: UserController;
@@ -15,16 +16,25 @@ describe("UserController", () => {
   });
 
   it("should can say hello", async () => {
-    const response =await  controller.sayHello(
+    const response = await controller.sayHello(
       "eko",
       "khanedy"
     );
     expect(response).toBe("Hello eko and khanedy");
   });
   it("should can say Hola", async () => {
-    const response =await  controller.getHola();
+    const response = await controller.getHola();
     expect(response).toBe("Hola");
   });
 
+  it("should can view hello", async () => {
+    const response = httpmock.createResponse();
+    controller.viewHello("Eko", response);
+    expect(response._getRenderView()).toBe("index.html");
+    expect(response._getRenderData()).toEqual({
+      name: "Eko",
+      title: "Template Engine"
+    });
 
+  });
 });
